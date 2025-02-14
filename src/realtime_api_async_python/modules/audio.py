00e03fb13c2ef41e17b89a05939492ad63e1ecd4
noog6 <ncogghe@gmail.com>
2025-02-14 18:32:03 -0500

    Adding a bit of silence before sounds made to get rid of some audio artifacts that were being noticed.

--- src/realtime_api_async_python/modules/audio.py
+++ src/realtime_api_async_python/modules/audio.py
@@ -7,10 +7,20 @@ from .utils import FORMAT, CHANNELS, RATE
 async def play_audio(audio_data):
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+    
+    # Add a small delay of silence at the start to prevent popping, and weird crackling
+    silence_duration = 0.4
+    silence_frames = int(RATE * silence_duration)
+    silence = b"\x00" * (
+        silence_frames * CHANNELS * 2
+    )  # 2 bytes per sample for 16-bit audio
+    stream.write(silence)
+
+    # Write our actual audio out
     stream.write(audio_data)
 
     # Add a small delay of silence at the end to prevent popping, and weird cuts off sounds
-    silence_duration = 0.4
+    silence_duration = 0.6
     silence_frames = int(RATE * silence_duration)
     silence = b"\x00" * (
         silence_frames * CHANNELS * 2
