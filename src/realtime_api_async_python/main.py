8f29980306e238c45d272bbc0c274a3bc5338ddc
noog6 <ncogghe@gmail.com>
2025-04-11 12:01:24 -0400

    Changing Theo's voice

--- src/realtime_api_async_python/main.py
+++ src/realtime_api_async_python/main.py
@@ -163,7 +163,7 @@ class RealtimeAPI:
             "session": {
                 "modalities": ["text", "audio"],
                 "instructions": SESSION_INSTRUCTIONS,
-                "voice": "verse",
+                "voice": "ballad",
                 "input_audio_format": "pcm16",
                 "output_audio_format": "pcm16",
                 "turn_detection": {
