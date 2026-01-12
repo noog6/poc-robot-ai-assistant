ecc6c18fb2cf6c57de83957ab37622bbacf167e3
noog6 <ncogghe@gmail.com>
2026-01-11 20:58:22 -0500

    Having fun with ascii art

--- src/realtime_api_async_python/main.py
+++ src/realtime_api_async_python/main.py
@@ -45,8 +45,14 @@ with open(os.getenv("PERSONALIZATION_FILE"), "r") as f:
     personalization = json.load(f)
 
 def main():
-    print(f"Starting realtime API...")
-    logger.info(f"Starting realtime API...")
+    logger.info("           _____                    _     _              _   ")
+    logger.info("     /\   |_   _|     /\           (_)   | |            | |  ")
+    logger.info("    /  \    | |      /  \   ___ ___ _ ___| |_ __ _ _ __ | |_ ")
+    logger.info("   / /\ \   | |     / /\ \ / __/ __| / __| __/ _` | '_ \| __|")
+    logger.info("  / ____ \ _| |_   / ____ \\__ \__ \ \__ \ || (_| | | | | |_ ")
+    logger.info(" /_/    \_\_____| /_/    \_\___/___/_|___/\__\__,_|_| |_|\__|")
+    logger.info("")
+    logger.info("Starting realtime API...")
     parser = argparse.ArgumentParser(
         description="Run the realtime API with optional prompts."
     )
@@ -55,17 +61,17 @@ def main():
     prompts = args.prompts.split("|") if args.prompts else None
     realtime_api_instance = RealtimeAPI(prompts)
     
-    print("Starting motion controller...")
+    logger.info("Starting motion controller...")
     motion_controller = MotionController.get_instance()
     motion_controller.start_control_loop()
 
-    print(f"Starting camera controller...")
+    logger.info("Starting camera controller...")
     camera_instance = CameraController.get_instance()
-    print("Starting vision thread...")
+    logger.info("Starting vision thread...")
     camera_instance.set_realtime_instance(realtime_api_instance)
     camera_instance.start_vision_loop(vision_loop_period_ms=1000)
     
-    print("Starting Awareness Engine...")
+    logger.info("Starting Awareness Engine...")
     awareness_engine = AwarenessEngine.get_instance()
     awareness_engine.set_realtime_instance(realtime_api_instance)
     #awareness_engine.start_control_loop(control_loop_period_ms=15000)
